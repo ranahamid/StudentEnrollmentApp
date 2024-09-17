8629817bf4d93860478139a8c8afaed871514dd3
Rana Hamid <rhamid@mymedicalhub.com>
2024-09-17 12:34:00 +0600

    cache

--- StudentEnrollment.API/Program.cs
+++ StudentEnrollment.API/Program.cs
@@ -11,9 +11,11 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using System.Text;
-
+using AspNetCoreRateLimit;
+using Marvin.Cache.Headers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,49 +30,71 @@ builder.Services.AddDbContext<StudentEnrollmentDbContext>(options =>
         );
 });
 
+builder.Services.AddMemoryCache();
+#region rate limiting
+
+builder.Services.AddInMemoryRateLimiting();
+
+var rateLimitRules = new List<RateLimitRule>
+{
+    new RateLimitRule
+    {
+        Endpoint = "*",
+        Limit = 10,
+        Period = "10s",
+    },
+    new RateLimitRule
+    {
+        Endpoint = "*",
+        Period = "1h",
+        Limit = 3600,
+    }
+};
+builder.Services.Configure<IpRateLimitOptions>(x =>
+{
+    x.GeneralRules = rateLimitRules;
+});
+
+builder.Services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+builder.Services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+#endregion
+
+builder.Services.AddResponseCaching();
+builder.Services.AddHttpCacheHeaders(x =>
+    {
+        x.MaxAge = 135;
+        x.CacheLocation = CacheLocation.Public;
+    },
+    y =>
+    {
+        y.MustRevalidate = true;
+    });
 
 builder.Services.AddIdentityCore<SchoolUser>()
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<StudentEnrollmentDbContext>();
-
  
-builder.Services.AddAuthentication(options => {
-        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer(options => {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
-            ValidateAudience = true,
-            ValidAudience = builder.Configuration["JwtSettings:Audience"],
-            ValidateLifetime = true,
-            ClockSkew = TimeSpan.Zero,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Key"]))
-        };
-    });
-
-//builder.Services.AddAuthentication(x =>
-//{
-//    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-//    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-//}).AddJwtBearer(x =>
-//{
-//    x.RequireHttpsMetadata = false;
-//    x.SaveToken = true;
-//    x.TokenValidationParameters = new TokenValidationParameters
-//    {
-//        ValidateIssuer = true,
-//        ValidateAudience = true, //missing in first 
-//        ValidateLifetime = true,
-//        ValidateIssuerSigningKey = true,
-//        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
-//        ValidAudience = builder.Configuration["JwtSettings:Audience"],//missing in first 
-//        ClockSkew = TimeSpan.Zero,//missing in first 
-//        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Key"]))
-//    };
-//});
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true, //missing in first 
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+        ValidAudience = builder.Configuration["JwtSettings:Audience"],//missing in first 
+        ClockSkew = TimeSpan.Zero,//missing in first 
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Key"]))
+    };
+});
 
 builder.Services.AddAutoMapper(typeof(MapperConfig));
 
@@ -82,44 +106,44 @@ builder.Services.AddAuthorization(options => {
 });
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
-//builder.Services.AddSwaggerGen(c =>
-//{
-//    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-//    {
-//        Description = @"JWT Authorization header using the Bearer scheme. 
-//                      Enter 'Bearer' [space] and then your token in the text input below.
-//                      Example: 'Bearer 12345abcdef'",
-//        Name = "Authorization",
-//        In = ParameterLocation.Header,
-//        Type = SecuritySchemeType.ApiKey,
-//        Scheme = "Bearer"
-//    });
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      Example: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
 
-//    c.AddSecurityRequirement(new OpenApiSecurityRequirement() {
-//                    {
-//                        new OpenApiSecurityScheme
-//                        {
-//                            Reference = new OpenApiReference {
-//                                Type = ReferenceType.SecurityScheme,
-//                                Id = "Bearer"
-//                            },
-//                            Scheme = "0auth2",
-//                            Name = "Bearer",
-//                            In = ParameterLocation.Header
-//                        },
-//                        new List<string>()
-//                    }
-//                });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement() {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "0auth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string>()
+                    }
+                });
 
 
-//    //c.SwaggerDoc("V1", new OpenApiInfo
-//    //{
-//    //    Version = "V1",
-//    //    Title = "StudentEnrollment.API"
-//    //});
-//});
+    //c.SwaggerDoc("V1", new OpenApiInfo
+    //{
+    //    Version = "V1",
+    //    Title = "StudentEnrollment.API"
+    //});
+});
 
 
 builder.Services.AddScoped<IStudentRepository, StudentRepository>();
